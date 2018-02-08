a5dcacbb5893a951d130fe00ba0cd460b2bd77c8
maditnerd <remi@madnerd.org>
2018-02-08 14:20:35 +0100

    test

--- sw.js
+++ sw.js
@@ -78,7 +78,7 @@ self.addEventListener('fetch',event => {
     const requestUrl = new URL(event.request.url);
     
     if(requestUrl.origin === location.origin){
-        if(requestUrl.href.slice(-1) === '/'){
+        if(requestUrl.pathname.slice(-1) === '/'){
             console.log("Loading cache");
 
             event.respondWith(
